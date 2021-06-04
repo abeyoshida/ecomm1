9200ced9dd275173e5b76859f4d9906d484b31ad
Abe Yoshida <abeyoshida@gmail.com>
2021-06-04 15:56:14 -0400

    Add checkout component, add cart items total cost selector

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { createStructuredSelector } from "reselect";
 
 import "./App.css";
 
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
@@ -50,6 +52,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
