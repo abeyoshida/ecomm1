a511553ae04df495c245bc4631ffde972a837696
Abe Yoshida <abeyoshida@gmail.com>
2021-07-06 16:01:22 -0400

    add Stripe checkout button component using Stripe's checkout flow

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JAHsnDxhs45TZq6rJJCO0SfNO5XmpHEdqHMIf5aSAeukx6kwW2pmvbFbcyQeA2ah49RpJexzYrnRVb1Xu1m2DYm00pRdx6MXS";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="eCom"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
